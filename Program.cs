2abe4e288fa9f1c9f4f69157a1d07c8bb8ce3105
yissachar shechter <upsilon01@gmail.com>
2022-04-26 01:52:04 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿var output = new List<LogRecord>();
+
+List<string> paths = Directory.EnumerateFiles(@"./", "*.txt").ToList();
+foreach (string filePath in paths)
+{
+    using (StreamReader reader = new StreamReader(filePath))
+    {
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            var logRecord = new LogRecord(line);
+            if (logRecord.level == "INFO")
+            {
+                output.Add(logRecord);
+            }
+        }
+    }
+}
+
+output.ForEach(Console.WriteLine);
+
+
+class LogRecord
+{
+    public LogRecord(string line)
+    {
+        string[] arr = line.Split('\t');
+        date = DateTime.Parse(arr[0]);
+        level = arr[1];
+        message = arr[2];
+    }
+    public DateTime date { get; set; }
+    public string message { get; set; }
+    public string level { get; set; }
+
+    public override string ToString()
+    {
+        return date + " " + message;
+    }
+}
+
